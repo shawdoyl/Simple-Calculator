783a10d5a95340e8cdb5f6ac3bbbea06288dea16
Doyle Shaw <shawdoyl@isu.edu>
2020-08-26 08:58:35 -0600

    First Commit

---
+++ SimpleCalculator/Form1.Designer.vb
@@ -0,0 +1,295 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CalculatorForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.InputTextBox = New System.Windows.Forms.TextBox()
+        Me.OneButton = New System.Windows.Forms.Button()
+        Me.TwoButton = New System.Windows.Forms.Button()
+        Me.ThreeButton = New System.Windows.Forms.Button()
+        Me.FourButton = New System.Windows.Forms.Button()
+        Me.FiveButton = New System.Windows.Forms.Button()
+        Me.SixButton = New System.Windows.Forms.Button()
+        Me.SevenButton = New System.Windows.Forms.Button()
+        Me.EightButton = New System.Windows.Forms.Button()
+        Me.NineButton = New System.Windows.Forms.Button()
+        Me.ZeroButton = New System.Windows.Forms.Button()
+        Me.EnterButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.AdditionButton = New System.Windows.Forms.Button()
+        Me.MultiplicationButton = New System.Windows.Forms.Button()
+        Me.SubtractButton = New System.Windows.Forms.Button()
+        Me.DivisionButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.ExponentButton = New System.Windows.Forms.Button()
+        Me.OutputListBox = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'InputTextBox
+        '
+        Me.InputTextBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.InputTextBox.Location = New System.Drawing.Point(16, 12)
+        Me.InputTextBox.Name = "InputTextBox"
+        Me.InputTextBox.Size = New System.Drawing.Size(276, 32)
+        Me.InputTextBox.TabIndex = 0
+        '
+        'OneButton
+        '
+        Me.OneButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.OneButton.Location = New System.Drawing.Point(16, 52)
+        Me.OneButton.Name = "OneButton"
+        Me.OneButton.Size = New System.Drawing.Size(88, 55)
+        Me.OneButton.TabIndex = 1
+        Me.OneButton.Text = "1"
+        Me.OneButton.UseVisualStyleBackColor = True
+        '
+        'TwoButton
+        '
+        Me.TwoButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TwoButton.Location = New System.Drawing.Point(110, 52)
+        Me.TwoButton.Name = "TwoButton"
+        Me.TwoButton.Size = New System.Drawing.Size(88, 55)
+        Me.TwoButton.TabIndex = 2
+        Me.TwoButton.Text = "2"
+        Me.TwoButton.UseVisualStyleBackColor = True
+        '
+        'ThreeButton
+        '
+        Me.ThreeButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ThreeButton.Location = New System.Drawing.Point(204, 52)
+        Me.ThreeButton.Name = "ThreeButton"
+        Me.ThreeButton.Size = New System.Drawing.Size(88, 55)
+        Me.ThreeButton.TabIndex = 3
+        Me.ThreeButton.Text = "3"
+        Me.ThreeButton.UseVisualStyleBackColor = True
+        '
+        'FourButton
+        '
+        Me.FourButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FourButton.Location = New System.Drawing.Point(16, 113)
+        Me.FourButton.Name = "FourButton"
+        Me.FourButton.Size = New System.Drawing.Size(88, 55)
+        Me.FourButton.TabIndex = 4
+        Me.FourButton.Text = "4"
+        Me.FourButton.UseVisualStyleBackColor = True
+        '
+        'FiveButton
+        '
+        Me.FiveButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FiveButton.Location = New System.Drawing.Point(110, 113)
+        Me.FiveButton.Name = "FiveButton"
+        Me.FiveButton.Size = New System.Drawing.Size(88, 55)
+        Me.FiveButton.TabIndex = 5
+        Me.FiveButton.Text = "5"
+        Me.FiveButton.UseVisualStyleBackColor = True
+        '
+        'SixButton
+        '
+        Me.SixButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SixButton.Location = New System.Drawing.Point(204, 113)
+        Me.SixButton.Name = "SixButton"
+        Me.SixButton.Size = New System.Drawing.Size(88, 55)
+        Me.SixButton.TabIndex = 6
+        Me.SixButton.Text = "6"
+        Me.SixButton.UseVisualStyleBackColor = True
+        '
+        'SevenButton
+        '
+        Me.SevenButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SevenButton.Location = New System.Drawing.Point(16, 174)
+        Me.SevenButton.Name = "SevenButton"
+        Me.SevenButton.Size = New System.Drawing.Size(88, 55)
+        Me.SevenButton.TabIndex = 7
+        Me.SevenButton.Text = "7"
+        Me.SevenButton.UseVisualStyleBackColor = True
+        '
+        'EightButton
+        '
+        Me.EightButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.EightButton.Location = New System.Drawing.Point(110, 174)
+        Me.EightButton.Name = "EightButton"
+        Me.EightButton.Size = New System.Drawing.Size(88, 55)
+        Me.EightButton.TabIndex = 8
+        Me.EightButton.Text = "8"
+        Me.EightButton.UseVisualStyleBackColor = True
+        '
+        'NineButton
+        '
+        Me.NineButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.NineButton.Location = New System.Drawing.Point(204, 174)
+        Me.NineButton.Name = "NineButton"
+        Me.NineButton.Size = New System.Drawing.Size(88, 55)
+        Me.NineButton.TabIndex = 9
+        Me.NineButton.Text = "9"
+        Me.NineButton.UseVisualStyleBackColor = True
+        '
+        'ZeroButton
+        '
+        Me.ZeroButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ZeroButton.Location = New System.Drawing.Point(110, 235)
+        Me.ZeroButton.Name = "ZeroButton"
+        Me.ZeroButton.Size = New System.Drawing.Size(88, 55)
+        Me.ZeroButton.TabIndex = 10
+        Me.ZeroButton.Text = "0"
+        Me.ZeroButton.UseVisualStyleBackColor = True
+        '
+        'EnterButton
+        '
+        Me.EnterButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.EnterButton.Location = New System.Drawing.Point(110, 357)
+        Me.EnterButton.Name = "EnterButton"
+        Me.EnterButton.Size = New System.Drawing.Size(88, 55)
+        Me.EnterButton.TabIndex = 11
+        Me.EnterButton.Text = "Enter"
+        Me.EnterButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ClearButton.Location = New System.Drawing.Point(16, 357)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(88, 55)
+        Me.ClearButton.TabIndex = 12
+        Me.ClearButton.Text = "Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'AdditionButton
+        '
+        Me.AdditionButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.AdditionButton.Location = New System.Drawing.Point(16, 235)
+        Me.AdditionButton.Name = "AdditionButton"
+        Me.AdditionButton.Size = New System.Drawing.Size(88, 55)
+        Me.AdditionButton.TabIndex = 15
+        Me.AdditionButton.Text = "+"
+        Me.AdditionButton.UseVisualStyleBackColor = True
+        '
+        'MultiplicationButton
+        '
+        Me.MultiplicationButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MultiplicationButton.Location = New System.Drawing.Point(16, 296)
+        Me.MultiplicationButton.Name = "MultiplicationButton"
+        Me.MultiplicationButton.Size = New System.Drawing.Size(88, 55)
+        Me.MultiplicationButton.TabIndex = 14
+        Me.MultiplicationButton.Text = "*"
+        Me.MultiplicationButton.UseVisualStyleBackColor = True
+        '
+        'SubtractButton
+        '
+        Me.SubtractButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.SubtractButton.Location = New System.Drawing.Point(204, 235)
+        Me.SubtractButton.Name = "SubtractButton"
+        Me.SubtractButton.Size = New System.Drawing.Size(88, 55)
+        Me.SubtractButton.TabIndex = 13
+        Me.SubtractButton.Text = "-"
+        Me.SubtractButton.UseVisualStyleBackColor = True
+        '
+        'DivisionButton
+        '
+        Me.DivisionButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DivisionButton.Location = New System.Drawing.Point(110, 296)
+        Me.DivisionButton.Name = "DivisionButton"
+        Me.DivisionButton.Size = New System.Drawing.Size(88, 55)
+        Me.DivisionButton.TabIndex = 18
+        Me.DivisionButton.Text = "/"
+        Me.DivisionButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ExitButton.Location = New System.Drawing.Point(204, 357)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(88, 55)
+        Me.ExitButton.TabIndex = 17
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'ExponentButton
+        '
+        Me.ExponentButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ExponentButton.Location = New System.Drawing.Point(204, 296)
+        Me.ExponentButton.Name = "ExponentButton"
+        Me.ExponentButton.Size = New System.Drawing.Size(88, 55)
+        Me.ExponentButton.TabIndex = 16
+        Me.ExponentButton.Text = "^"
+        Me.ExponentButton.UseVisualStyleBackColor = True
+        '
+        'OutputListBox
+        '
+        Me.OutputListBox.FormattingEnabled = True
+        Me.OutputListBox.Location = New System.Drawing.Point(298, 12)
+        Me.OutputListBox.Name = "OutputListBox"
+        Me.OutputListBox.Size = New System.Drawing.Size(196, 394)
+        Me.OutputListBox.TabIndex = 19
+        '
+        'CalculatorForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(506, 427)
+        Me.Controls.Add(Me.OutputListBox)
+        Me.Controls.Add(Me.DivisionButton)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.ExponentButton)
+        Me.Controls.Add(Me.AdditionButton)
+        Me.Controls.Add(Me.MultiplicationButton)
+        Me.Controls.Add(Me.SubtractButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.EnterButton)
+        Me.Controls.Add(Me.ZeroButton)
+        Me.Controls.Add(Me.NineButton)
+        Me.Controls.Add(Me.EightButton)
+        Me.Controls.Add(Me.SevenButton)
+        Me.Controls.Add(Me.SixButton)
+        Me.Controls.Add(Me.FiveButton)
+        Me.Controls.Add(Me.FourButton)
+        Me.Controls.Add(Me.ThreeButton)
+        Me.Controls.Add(Me.TwoButton)
+        Me.Controls.Add(Me.OneButton)
+        Me.Controls.Add(Me.InputTextBox)
+        Me.Name = "CalculatorForm"
+        Me.Text = "Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents InputTextBox As TextBox
+    Friend WithEvents OneButton As Button
+    Friend WithEvents TwoButton As Button
+    Friend WithEvents ThreeButton As Button
+    Friend WithEvents FourButton As Button
+    Friend WithEvents FiveButton As Button
+    Friend WithEvents SixButton As Button
+    Friend WithEvents SevenButton As Button
+    Friend WithEvents EightButton As Button
+    Friend WithEvents NineButton As Button
+    Friend WithEvents ZeroButton As Button
+    Friend WithEvents EnterButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents AdditionButton As Button
+    Friend WithEvents MultiplicationButton As Button
+    Friend WithEvents SubtractButton As Button
+    Friend WithEvents DivisionButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents ExponentButton As Button
+    Friend WithEvents OutputListBox As ListBox
+End Class
---
+++ SimpleCalculator/Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class CalculatorForm
+    Private Sub CalculatorForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
